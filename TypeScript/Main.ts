b027430f8e37a65939694f8a813c016aeeb37fb0
Felix Mayer <FelixM@yer.tools>
2022-10-27 08:56:29 +0200

    Test automatic container update

--- TypeScript/Main.ts
+++ TypeScript/Main.ts
@@ -73,6 +73,10 @@ function backgroundAnimation(){
 
 backgroundAnimation();
 
+if(typeof window === "undefined"){
+    console.log("window can't be undefined ;)")
+}
+
 /*const photographyGalleryContainer = document.getElementById("photography-gallery-container");
 if(photographyGalleryContainer){
     const imageGallary = new Libraries.ImageGallary("/DotNet/wwwroot/jpg/full-size/", "/DotNet/wwwroot/jpg/", ".jpg", 39);
